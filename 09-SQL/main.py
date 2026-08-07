41d307b65ff71ef3d9e79981b7da15dcf2b98d1f
ifrit1066 <10705089+alewington@users.noreply.github.com>
2026-08-07 15:17:37 +0100

    new files

---
+++ 09-SQL/main.py
@@ -0,0 +1,35 @@
+# SQL Getting Started
+
+# SQL is a standard language for storing, manipulating, and retrieving data in
+# databases. It is used to communicate with a database. SQL is used to perform
+# tasks such as updating data on a database, or retrieving data from a
+# database. Some common relational database management systems that use SQL
+# are: Oracle, Sybase, Microsoft SQL Server, Access, Ingres, etc.
+
+# To install SQL, you can use the following command in your terminal:
+# with pip:
+# pip install sql
+# With conda:
+# conda install sql
+
+# You will require the following packages installed: sql, pandas, plotly &
+# flask.
+
+# To check the version of SQL installed, run the following command in your
+# terminal:
+# python -c "import sql; print(sql.__version__)"
+
+# To initiate SQL, run the following command in your terminal:
+# import sql
+
+# You will require a database management system (DBMS) to run SQL queries.
+
+# Some common DBMS are:
+# MySQL, PostgreSQL, SQLite, Oracle, Microsoft SQL Server, etc.
+
+# some database management systems are free to use.
+# some databases are systemless and can be used without installing a DBMS.
+
+# Examples of these are MongoDB, SQLite, DuckDB, and Apache Derby.
+# These databases are lightweight and can be used for small to medium-sized
+# applications.
